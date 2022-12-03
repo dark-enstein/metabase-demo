d211ec3dd7468c3b2081224375b5ff8e98bd251d
Darkenstein.R <darkenstein.r@gmail.com>
2022-12-03 15:08:26 +0100

    init metabase demo

---
+++ startup.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+#Download metabase jar
+echo "1) Dowloading Metabase v0.44.6..."
+wget https://downloads.metabase.com/v0.44.6/metabase.jar
+echo Download complete...
+
+#start dockerfile
+
